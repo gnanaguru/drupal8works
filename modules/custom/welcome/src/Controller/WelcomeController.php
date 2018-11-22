5387b519e72c03643ec7faa0039a8e98c390ed36
Gnanaguru Mariydass <gnanaguru.mariyadass@keypathedu.com>
2018-11-22 20:00:16 +0530

    welcome module

---
+++ modules/custom/welcome/src/Controller/WelcomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\welcome\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class WelcomeController extends ControllerBase{
+
+	public function index(){
+		return 'Welcome message list page ';
+	}
+}
